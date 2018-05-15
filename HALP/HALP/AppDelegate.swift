c0dbdaeb4a35b386620a3d79f7686bdf00fcdc03
qileng <qileng@ucsd.edu>
2018-05-15 13:59:22 -0700

    Added Azure framework.

--- HALP/HALP/AppDelegate.swift
+++ HALP/HALP/AppDelegate.swift
@@ -16,6 +16,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	var window: UIWindow?
+	var client: MSClient?
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 		// Override point for customization after application launch.
---
+++ HALP/HALP/Header.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  HALP
+//
+//  Created by Qihao Leng on 5/15/18.
+//  Copyright © 2018 Team Zero. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+#import <MicrosoftAzureMobile/MicrosoftAzureMobile.h>
+
+#endif /* Header_h */
