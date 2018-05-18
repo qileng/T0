a619f4a0f207fdcf1dbb6fdb4089c9eca6e709d3
Dong Yoon <hdy7611@gmail.com>
2018-05-18 11:18:36 -0700

    TaskEdit commit. I will work on more details

---
+++ HALP/HALP/TaskDetailTableViewController.swift
@@ -0,0 +1,48 @@
+//
+//  TaskDetailTableViewController.swift
+//  HALP
+//
+//  Created by Dong Yoon Han on 5/18/18.
+//  Copyright © 2018 Team Zero. All rights reserved.
+//
+
+import UIKit
+
+protocol  TaskDetailTableViewControllerDelegate{
+    func changeDetail(text label:String, indexPath:IndexPath)
+}
+
+class TaskDetailTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+
+    @IBOutlet weak var tableViewOutlet: UITableView!
+    var cellData:[String]?
+    var delegate:TaskDetailTableViewControllerDelegate?
+    var selectedIndexPath:IndexPath?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableViewOutlet.tableFooterView = UIView()
+        self.tableViewOutlet.backgroundColor = .clear
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cellData?.count ?? 0
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "basicCell", for: indexPath)
+        cell.textLabel?.text = cellData?[indexPath.row]
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        let text = cellData?[indexPath.row] ?? ""
+        self.delegate?.changeDetail(text: text, indexPath: self.selectedIndexPath!)
+        self.navigationController?.popViewController(animated: true)
+    }
+}
---
+++ HALP/HALP/TaskEditPageViewController.swift
@@ -0,0 +1,279 @@
+//
+//  TaskEditPageViewController.swift
+//  HALP
+//
+//  Created by Dong Yoon Han on 5/16/18.
+//  Copyright © 2018 Team Zero. All rights reserved.
+//
+
+import UIKit
+
+enum CellTypes:String {
+    case dateDetail = "dateDetailCell"
+    case textField = "textFieldCell"
+    case picker = "pickerCell"
+    case datePicker = "datePickerCell"
+    case detail = "detailCell"
+    case textView = "textViewCell"
+    case basic = "basicCell"
+}
+struct CellData {
+    var cellType:CellTypes
+    var title:String
+    var detail:String?
+    var date:Date?
+//    var category:Category?
+}
+
+class TaskEditPageViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableViewOutlet: UITableView!
+    
+    var task:Task?
+    var datePickerIndexPath: IndexPath?
+    var fieldData:[[CellData]] = [[]]
+    var dateFormatter = DateFormatter()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .short
+        tableViewOutlet.backgroundColor = .clear
+//        tableViewOutlet.estimatedRowHeight = 45
+//        tableViewOutlet.rowHeight = UITableViewAutomaticDimension
+//        tableViewOutlet.sectionHeaderHeight = UITableViewAutomaticDimension
+        //remove empty cells in tableview
+        tableViewOutlet.tableFooterView = UIView()
+
+        dateFormatter.dateFormat = "MMMM dd, yyyy HH:mm a"
+        
+        let startDateStr = dateFormatter.string(from: Date())
+        guard let date = Calendar.current.date(byAdding: .hour, value: 1, to: Date()) else{return}
+        let endDateStr = dateFormatter.string(from: date)
+        
+        let section0 = [CellData(cellType: .textField, title: "Title", detail: nil, date: nil)]
+        let section1 =
+            [CellData(cellType: .dateDetail, title: "Starts", detail: startDateStr, date: Date()),
+             CellData(cellType: .dateDetail, title: "Ends", detail: endDateStr, date: date)]
+        let section2 =
+            [CellData(cellType: .detail, title: "Category", detail: "", date: nil),
+             CellData(cellType: .detail, title: "Alert", detail: "", date: nil)]
+        let section3 = [CellData(cellType: .textView, title: "Description", detail: nil, date: nil)]
+        fieldData = [ section0, section1, section2, section3]
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = true
+        self.tableViewOutlet.reloadData()
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = false
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return fieldData.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        var numberOfRows = fieldData[section].count
+        if datePickerIndexPath != nil && fieldData[section][0].cellType == CellTypes.dateDetail
+        {
+            numberOfRows += 1
+        }
+        return numberOfRows
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell?
+        if datePickerIndexPath != nil && datePickerIndexPath?.section == indexPath.section
+                                    && datePickerIndexPath!.row == indexPath.row
+        {
+            cell = tableView.dequeueReusableCell(withIdentifier: CellTypes.datePicker.rawValue, for: indexPath)
+            let datePicker = cell?.viewWithTag(99) as! UIDatePicker
+
+            if let date = fieldData[indexPath.section][indexPath.row-1].date
+            {
+                datePicker.setDate(date, animated: true)
+            }
+            return cell!
+        }
+
+        let celltype = self.fieldData[indexPath.section][indexPath.row].cellType
+        switch (celltype)
+        {
+        case .textField:
+            cell = tableView.dequeueReusableCell(withIdentifier: celltype.rawValue, for: indexPath)
+        case .dateDetail:
+            cell = tableView.dequeueReusableCell(withIdentifier: celltype.rawValue, for: indexPath)
+            cell?.textLabel?.text = fieldData[indexPath.section][indexPath.row].title
+            cell?.detailTextLabel?.text = fieldData[indexPath.section][indexPath.row].detail
+            //        case .datePicker:
+        //            cell = tableView.dequeueReusableCell(withIdentifier: celltype.rawValue, for: indexPath)
+        case .picker:
+            cell = tableView.dequeueReusableCell(withIdentifier: celltype.rawValue, for: indexPath)
+        case .detail:
+            cell = tableView.dequeueReusableCell(withIdentifier: celltype.rawValue, for: indexPath)
+            cell?.textLabel?.text = fieldData[indexPath.section][indexPath.row].title
+            cell?.detailTextLabel?.text = fieldData[indexPath.section][indexPath.row].detail
+        case .textView:
+            cell = tableView.dequeueReusableCell(withIdentifier: celltype.rawValue, for: indexPath)
+        default:
+            cell = UITableViewCell()
+        }
+        return cell!
+        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        var rowHeight = tableView.rowHeight
+        var rowHeight = UITableViewAutomaticDimension
+        if indexPath.section == 1
+        {
+            if datePickerIndexPath != nil && datePickerIndexPath!.row == indexPath.row
+            {
+                rowHeight = tableView.dequeueReusableCell(withIdentifier: CellTypes.datePicker.rawValue)?.frame.height ?? rowHeight
+                return rowHeight
+            }
+        }else if (indexPath.section == 0) || (indexPath.section == 2)
+        {
+            rowHeight = 44
+        }else if indexPath.section == 3
+        {
+            rowHeight = 128
+        }
+        return rowHeight
+    }
+    /*
+     When we tap a row, tableView:didSelectRowAtIndexPath: is called. There are three cases:
+   1.  There is no date picker shown, we tap a row, then a date picker is shown just under it.
+   2.  A date picker is shown, we tap the row just above it, then the date picker is hidden.
+   3.  A date picker is shown, we tap a row that is not just above it, then the date picker is hidden and another date picker under the tapped row is shown. And there are two subcases:
+        1) the tapped is above the shown date picker
+        2) the tapped is under the shown date picker
+     */
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if indexPath.section == 1 // the selected row is in section 1
+        {
+            let selectedCell = tableViewOutlet.cellForRow(at: indexPath)
+            tableView.beginUpdates()
+
+            if datePickerIndexPath != nil && datePickerIndexPath!.row - 1 == indexPath.row
+            { // case 2
+                tableView.deleteRows(at: [datePickerIndexPath!], with: .fade)
+                datePickerIndexPath = nil
+                selectedCell?.detailTextLabel?.textColor = .black
+            }else
+            { // case 1, 3
+                if datePickerIndexPath != nil
+                { // case 3
+                    let cell = tableViewOutlet.cellForRow(at: IndexPath(row: (datePickerIndexPath?.row)!-1, section: indexPath.section))
+                    cell?.detailTextLabel?.textColor = .black
+                    tableView.deleteRows(at: [datePickerIndexPath!], with: .fade)
+                }
+                selectedCell?.detailTextLabel?.textColor = .red
+                datePickerIndexPath = calculateDatePickerIndexPath(indexPathSelected: indexPath)
+                tableView.insertRows(at: [datePickerIndexPath!], with: .fade)
+            }
+            tableView.deselectRow(at: indexPath, animated: false)
+            tableView.endUpdates()
+        }else if indexPath.section == 2 // the selected row is in section 2
+        {
+            let detailVC = self.storyboard?.instantiateViewController(withIdentifier: "TaskDetailTableViewController") as! TaskDetailTableViewController
+            if fieldData[indexPath.section][indexPath.row].title == "Category"
+            {
+                detailVC.cellData = ["Study", "Work", "Entertainment", "Chore", "Social"]
+            }else
+            { // "Alert"
+                detailVC.cellData = ["None", "At start time of Event", "5 minutes before", "5 minutes before",
+                "10 minutes before", "15 minutes before", "30 minutes before",
+                "1 Hours before", "2 Hours before"]
+            }
+            detailVC.selectedIndexPath = indexPath
+            detailVC.delegate = self
+            self.navigationController?.pushViewController(detailVC, animated: true)
+            tableView.deselectRow(at: indexPath, animated: false)
+        }
+//        tableViewOutlet.reloadData()
+//        tableView.deselectRow(at: indexPath, animated: false)
+    }
+    
+    func calculateDatePickerIndexPath(indexPathSelected: IndexPath) -> IndexPath
+    {
+        if datePickerIndexPath != nil && datePickerIndexPath!.row  <= indexPathSelected.row
+        { // case 3.2
+            return IndexPath(row: indexPathSelected.row, section: indexPathSelected.section)
+        }else
+        {
+            // case 1, 3.1
+            return IndexPath(row: indexPathSelected.row+1, section: indexPathSelected.section)
+        }
+    }
+    
+ 
+    @IBAction func datePickerValueChanged(_ sender: UIDatePicker) {
+        let parentIndexPath = IndexPath(row: datePickerIndexPath!.row-1, section: datePickerIndexPath!.section)
+        
+        let dateCell = tableViewOutlet.cellForRow(at: parentIndexPath)
+//        var celldata = fieldData[parentIndexPath.section][parentIndexPath.row]
+//        celldata.date = sender.date
+//        celldata.detail = dateFormatter.string(from: sender.date)
+        fieldData[parentIndexPath.section][parentIndexPath.row].date = sender.date
+        fieldData[parentIndexPath.section][parentIndexPath.row].detail = dateFormatter.string(from: sender.date)
+        dateCell?.detailTextLabel?.text = dateFormatter.string(from: sender.date)
+//        self.tableViewOutlet.reloadData()
+    }
+}
+
+extension TaskEditPageViewController : UITextViewDelegate, UITextFieldDelegate
+{
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.text == "Description"
+        {
+            textView.text = ""
+            textView.textColor = .black
+            textView.font = UIFont.systemFont(ofSize: 15)
+        }
+    }
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if text == "\n"
+        {
+            textView.resignFirstResponder()
+        }
+        return true
+    }
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text == ""
+        {
+            textView.text = "Description"
+            textView.textColor = .lightGray
+            textView.font = UIFont.systemFont(ofSize: 17)
+        }
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
+extension TaskEditPageViewController : TaskDetailTableViewControllerDelegate
+{
+    func changeDetail(text label:String, indexPath:IndexPath)
+    {
+        self.fieldData[indexPath.section][indexPath.row].detail = label
+//        let cell = self.tableViewOutlet.cellForRow(at: indexPath)
+//        cell?.detailTextLabel?.text = label
+    }
+}
+//extension TaskEditPageViewController: UIPickerViewDelegate, UIPickerViewDataSource
+//{
+//    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+//        <#code#>
+//    }
+//
+//    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+//        <#code#>
+//    }
+//
+//
+//}
