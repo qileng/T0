e386d217b38fae646a5bb1737fb8722a7e769ef2
Flik <hah045@ucsd.edu>
2018-05-18 10:42:56 -0700

    fix login (#67)

--- HALP/HALP/Startup.swift
+++ HALP/HALP/Startup.swift
@@ -231,27 +231,6 @@ class StartupViewController: UIViewController, UITextFieldDelegate, UIGestureRec
     
     // UI actions
     // Todo: comment
-    @objc func onDrage(_ sender:UIPanGestureRecognizer) {
-        let percentThreshold:CGFloat = 0.3
-        let translation = sender.translation(in: view)
-        
-        let newY = ensureRange(value: view.frame.minY + translation.y, minimum: 0, maximum: view.frame.maxY)
-        let progress = progressAlongAxis(newY, view.bounds.height)
-        
-        view.frame.origin.y = newY //Move view to new position
-        
-        if sender.state == .ended {
-            let velocity = sender.velocity(in: view)
-            if velocity.y >= 300 || progress > percentThreshold {
-                self.dismiss(animated: true) //Perform dismiss
-            } else {
-                UIView.animate(withDuration: 0.2, animations: {
-                    self.view.frame.origin.y = 0 // Revert animation
-                })
-            }
-        }
-        sender.setTranslation(.zero, in: view)
-    }
     
     func progressAlongAxis(_ pointOnAxis: CGFloat, _ axisLength: CGFloat) -> CGFloat {
         let movementOnAxis = pointOnAxis / axisLength
@@ -338,7 +317,6 @@ class StartupViewController: UIViewController, UITextFieldDelegate, UIGestureRec
         super.viewDidLoad()
         
         // view.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector( ) ))
-        view.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(onDrage)))
         
         self.navigationController?.isNavigationBarHidden = true
         setUpSubViewsLayout()
