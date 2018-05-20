f78ef1c610892c0b87d98bd1c9d94ec2bb8f5a81
qileng <qileng@ucsd.edu>
2018-05-20 13:19:26 -0700

    Setting (#77)
    * Fixed nil user bug.
    
    * Fixed schedule start time not logged in.
    
    * Fixed copy constructor in setting.
    
    * Variables added.
    
    * Setting page first two toggles
    
    * settings with four added toggles
    
    * setting DAO
    
    * Added setting connection codes except for notificationOn.
    
    * fix type
    
    * duh
    
    * Removed default view.
    
    * Added seven toggles to setting page
    
    * AvailableDays Toggles
    Uncomment guest login

--- HALP/HALP/SettingPage.swift
+++ HALP/HALP/SettingPage.swift
@@ -12,10 +12,10 @@ import UIKit
 class SettingViewController: UIViewController {
 	
 	@IBOutlet weak var ViewLabel: UILabel!
-	
-	var count = 0
+    
 	var viewName = "Setting Page"
-	
+    var settingForm: SettingForm?
+    
 	override func viewDidLoad() {
 		super.viewDidLoad()
 	}
@@ -26,13 +26,111 @@ class SettingViewController: UIViewController {
 	
 	override func viewWillAppear(_ animated: Bool) {
 		super.viewWillAppear(animated)
-		count += 1
-		self.ViewLabel!.text = viewName + " appeared \(count) time" + ((count == 1) ? "" : "s")
+        //Create a settingForm object
+        settingForm = SettingForm(TaskManager.sharedTaskManager.getSetting())
 	}
+    
     @IBAction func Logout(_ sender: Any) {
         let loginVC:StartupViewController = self.storyboard?.instantiateViewController(withIdentifier: "StartupViewController") as! StartupViewController
         let loginSignUpNC: UINavigationController = UINavigationController(rootViewController: loginVC)
         self.present(loginSignUpNC, animated: true, completion: nil)
     }
+    
+    @IBAction func notificationSwitch(_ sender: UISwitch) {
+        settingForm?.toggleNotification()
+    }
+    
+    @IBAction func defaultViewSegControl(_ sender: UISegmentedControl) {
+        if (sender.selectedSegmentIndex == 0){
+            //Switch to Clock View
+            settingForm?.setDefaultView(View(rawValue: 0)!)
+        } else if (sender.selectedSegmentIndex == 1) {
+            //Switch to List View
+            settingForm?.setDefaultView(View(rawValue: 1)!)
+        }
+    }
+    
+    @IBAction func defaultSortingMethodSegControl(_ sender: UISegmentedControl) {
+        if (sender.selectedSegmentIndex == 0){
+            //Switch to Time based sorting
+            settingForm?.setDefaultSort(SortingType(rawValue: 0)!)
+            print(settingForm?.getDefaultSort().rawValue)
+        } else if (sender.selectedSegmentIndex == 1) {
+            //Switch to Prority based sorting
+            settingForm?.setDefaultSort(SortingType(rawValue: 1)!)
+            print(settingForm?.getDefaultSort().rawValue)
+        }
+    }
+    
+    @IBAction func themeSegControl(_ sender: UISegmentedControl) {
+        if (sender.selectedSegmentIndex == 0){
+            //Switch to Light Theme
+            settingForm?.setTheme(Theme(rawValue: 0)!)
+            print(settingForm?.getTheme().rawValue)
+        } else if (sender.selectedSegmentIndex == 1){
+            //Switch to Dark Theme
+            settingForm?.setTheme(Theme(rawValue: 1)!)
+            print(settingForm?.getTheme().rawValue)
+        }
+    }
+    
+    //Seven toggles
+    @IBAction func sunSwitch(_ sender: UISwitch) {
+        if (sender.isOn == true){
+            settingForm?.setAvailableDays((settingForm?.getAvailableDays())! | 1<<0)
+        } else {
+            settingForm?.setAvailableDays((settingForm?.getAvailableDays())! & 0b1111110)
+        }
+    }
+    
+    @IBAction func monSwitch(_ sender: UISwitch) {
+        if (sender.isOn == true){
+            settingForm?.setAvailableDays((settingForm?.getAvailableDays())! | 1<<1)
+        } else {
+            settingForm?.setAvailableDays((settingForm?.getAvailableDays())! & 0b1111101)
+        }
+    }
+    
+    @IBAction func tueSwitch(_ sender: UISwitch) {
+        if (sender.isOn == true){
+            settingForm?.setAvailableDays((settingForm?.getAvailableDays())! | 1<<2)
+        } else {
+            settingForm?.setAvailableDays((settingForm?.getAvailableDays())! & 0b1111011)
+        }
+    }
+    
+    @IBAction func wedSwitch(_ sender: UISwitch) {
+        if (sender.isOn == true){
+            settingForm?.setAvailableDays((settingForm?.getAvailableDays())! | 1<<3)
+        } else {
+            settingForm?.setAvailableDays((settingForm?.getAvailableDays())! & 0b1110111)
+        }
+    }
+    
+    @IBAction func thuSwitch(_ sender: UISwitch) {
+        if (sender.isOn == true){
+            settingForm?.setAvailableDays((settingForm?.getAvailableDays())! | 1<<4)
+        } else {
+            settingForm?.setAvailableDays((settingForm?.getAvailableDays())! & 0b1101111)
+        }
+    }
+    
+    @IBAction func friSwitch(_ sender: UISwitch) {
+        if (sender.isOn == true){
+            settingForm?.setAvailableDays((settingForm?.getAvailableDays())! | 1<<5)
+        } else {
+            settingForm?.setAvailableDays((settingForm?.getAvailableDays())! & 0b1011111)
+        }
+    }
+    
+    @IBAction func satSwitch(_ sender: UISwitch) {
+        if (sender.isOn == true){
+            settingForm?.setAvailableDays((settingForm?.getAvailableDays())! | 1<<6)
+        } else {
+            settingForm?.setAvailableDays((settingForm?.getAvailableDays())! & 0b0111111)
+        }
+        print(settingForm?.getAvailableDays())
+    }
+    
 }
 
