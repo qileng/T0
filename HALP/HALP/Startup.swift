1713fbdbf705d08ad04e688af4dbd8d80b14fe0f
zach <zis037@ucsd.edu>
2018-05-20 12:27:19 -0700

    AvailableDays Toggles Uncomment guest login

--- HALP/HALP/Startup.swift
+++ HALP/HALP/Startup.swift
@@ -220,7 +220,7 @@ class StartupViewController: UIViewController, UITextFieldDelegate, UIGestureRec
 			TaskManager.sharedTaskManager.setUp(new: guest, setting: Setting(), caller: self as UIViewController)
 			
             self.dismiss(animated: true, completion: nil)
-//            self.present((self.storyboard?.instantiateViewController(withIdentifier: "RootViewController"))!, animated: true, completion: nil)
+            self.present((self.storyboard?.instantiateViewController(withIdentifier: "RootViewController"))!, animated: true, completion: nil)
         } catch {
             //There should not be any authentication error with guest login
             //All error should be directed here
