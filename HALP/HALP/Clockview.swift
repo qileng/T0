98421914136363e63c9254fae347e78886ecfbfd
qileng <qileng@ucsd.edu>
2018-05-29 00:09:19 -0700

    Clockview almost done. (#90)
    * Developed algorithm. Need to present tasks. Also 3 buttons don't work lol. pls help
    
    * collections started
    
    * Clock View 4.0 :)
    
    * Adding task detail. Working in progress.
    
    * Detail on ClockView almost done. Merging TESTING into current branch.
    
    * Task Detail on ClockView done.

--- HALP/HALP/Clockview.swift
+++ HALP/HALP/Clockview.swift
@@ -10,12 +10,163 @@
 
 import UIKit
 import CoreGraphics
+extension ClockViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+	// Protocal: UICollectionViewDataSource
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return clockTasks[currentIndex].count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "clockTaskCell", for: indexPath) as! ClockTaskCell
+        
+        let task = clockTasks[currentIndex][indexPath.row]
+        cell.displayContent(task: task)
+        
+		return cell
+    }
+	
+	// Protocal: UICollectionViewDelegate
+	func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+		// Retrieve the cell selected
+		let cell = collectionView.cellForItem(at: indexPath)!
+		// Retrieve the task selected
+		let task = clockTasks[currentIndex][indexPath.row]
+		
+		// Temporarily remove the container view of the clock hands
+		self.removeContainerView()
+		// Pop detail page
+		// Calculate frame position since cell.frame is so dumb that it won't give me the correct position
+		let originFrame = self.calculateFrame(collectionView, cell)
+		let targetFrame = CGRect(x: self.view.frame.width*0.1, y: self.view.frame.width*0.1, width: self.view.frame.width*0.8, height: self.view.frame.height*0.8)
+		let Detail = UITaskDetail(origin: originFrame, target: targetFrame, task: task)
+		self.view.addSubview(Detail)
+		// Setup tap recognizer
+		let TapRecognizer = UITapGestureRecognizer(target: self, action: #selector(onTapWhileDetailDisplayed))
+		self.view.addGestureRecognizer(TapRecognizer)
+	}
+	
+	func calculateFrame(_ collectionView: UICollectionView, _ cell: UICollectionViewCell) -> CGRect {
+		// There is no good way to determine a cell's position. Their order in the array seems to be highly dependent on the speed user scroll the collectionView.
+		// Here we use a trial&fail method.
+		// IMPORTANT: All calculation based on the fact that margin and padding are the same in all direction. This will certainly fail as soon as inconsistency occurs.
+		let index = collectionView.indexPath(for: cell)!.row
+		let origin: CGPoint
+		let padding: CGFloat
+		var row = 0.0 as CGFloat
+		var column = 0.0 as CGFloat
+		// First, for all cells with even indexPath, i.e. on the left.
+		if index % 2 == 0 {
+			// Try the cell above it
+			let trialCell = collectionView.cellForItem(at: IndexPath(row: index - 2, section: 0))
+			padding = cell.frame.origin.x
+			// If the cell above it is visible, it means the cell is in second row and first column, which is the third visible cell.
+			// Else it's the first visible cell
+			if trialCell != nil {
+				row = 1.0
+				column = 0.0
+			} else {
+				row = 0.0
+				column = 0.0
+			}
+		} else {
+			// Same thing for all cells on the right.
+			let trialCell = collectionView.cellForItem(at: IndexPath(row: index - 2, section: 0))
+			padding = (cell.frame.origin.x - cell.frame.width) / 2
+			// If the cell above it is visible, it means the cell is in second row and second column, which is the fourth visible cell.
+			// Else it's the second visible cell
+			if trialCell != nil {
+				row = 1.0
+				column = 1.0
+			} else {
+				row = 0.0
+				column = 1.0
+			}
+		}
+		// Calculate frame position accordingly
+		origin = CGPoint(x: padding + column * (padding + cell.frame.width) + collectionView.frame.origin.x, y: padding + row * (padding + cell.frame.height) + collectionView.frame.origin.y)
+		return CGRect(origin: origin, size: cell.frame.size)
+	}
+}
 
 class ClockViewController: UIViewController, CAAnimationDelegate {
 
-    @IBOutlet var timeButtons: [UIButton]!
+    var clockTasks = Array(repeating: [Task](), count: 12)
+    var currentIndex = 0
     
-
+    var taskCollection: UICollectionView!
+    
+    @IBOutlet var displayLabel: UILabel!
+    @IBOutlet var timeButtons: [UIButton]!
+	
+	// Listener to the buttons
+    @IBAction func onTap(_ sender: UIButton) {
+        var offset = Int(Date().timeIntervalSince1970)
+        offset = offset - (offset%3600)
+        let cal = Calendar.current
+        let startOfDay = cal.startOfDay(for: Date())
+        offset = offset - Int(startOfDay.timeIntervalSince1970)
+        offset = (offset / 3600) % 12
+        
+        let idx = (12+(sender.tag - offset))%12
+        currentIndex = idx
+        
+        //Collection view declaration
+        if taskCollection != nil {
+            taskCollection.removeFromSuperview()
+        }
+        
+        //Layout setup
+        let padding = CGFloat(10.0)
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = padding
+        layout.minimumInteritemSpacing = padding
+        layout.itemSize = CGSize(width: ((displayLabel.frame.width-padding)-(3*padding))/2, height: ((displayLabel.frame.height-padding)-(3*padding))/2)
+        let insets = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        layout.sectionInset = insets
+        
+        taskCollection = UICollectionView(frame: displayLabel.frame, collectionViewLayout: layout)
+        taskCollection.register(ClockTaskCell.self, forCellWithReuseIdentifier: "clockTaskCell")
+        taskCollection.dataSource = self
+        taskCollection.delegate = self
+        
+        taskCollection.backgroundColor = UIColor(hex: 0xce8964) //Needs to be color coded by category
+        
+        self.view.addSubview(taskCollection)
+        taskCollection.anchor(top: nil, left: nil, right: nil, bottom: nil, topConstant: 0, leftConstant: 0, rightConstant: 0, bottomConstant: 0, width: displayLabel.frame.width - 10, height: displayLabel.frame.height - 10, centerX: displayLabel.centerXAnchor, centerY: displayLabel.centerYAnchor)
+        
+        //print("index is ",idx, " offset is ", offset)
+        /*if (clockTasks[idx].count == 0) {
+           // message.text = "No Tasks"
+        } else {
+            for tasks in clockTasks[idx] {
+                //print("Task: " + tasks.getTitle())
+                //message.text = "Task: " + tasks.getTitle()
+            }
+        }*/
+    }
+    
+	// Listener used when detail page is poped
+	@objc func onTapWhileDetailDisplayed(_ sender: UITapGestureRecognizer) {
+		// Retrieve the detail view
+		let detailView = self.view.subviews.last!
+		// Determine the tapping location
+		let location = sender.location(in: detailView)
+		// If tapped outside the detail view
+		if !detailView.point(inside: location, with: nil) {
+			// Dismiss the detail view with animation
+			let animator = UIViewPropertyAnimator(duration: 0.1, curve: .easeOut, animations: UITaskDetail.dimiss(detailView as! UITaskDetail))
+			animator.addCompletion({_ in
+				self.view.subviews.last!.removeFromSuperview()
+				// Put clock hands back
+				self.addHandsAndCenterPiece()
+				// Remove the gesture recognizer
+				_ = self.view.gestureRecognizers!.popLast()
+			})
+			animator.startAnimation()
+		}
+	}
+    
     //The following commented code is for reference
     /*
     @IBOutlet var b1: UIButton! //Button for 11-12 region of clock
@@ -77,39 +228,82 @@ class ClockViewController: UIViewController, CAAnimationDelegate {
         NotificationCenter.default.addObserver(self, selector: #selector(applicationDidBecomeActive), name:NSNotification.Name.UIApplicationDidBecomeActive, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(applicationDidEnterBackground), name:NSNotification.Name.UIApplicationDidEnterBackground, object: nil)
 
-        let startAngle = M_PI/12
-        let angle = M_PI/6
+        let startAngle = .pi / 12.0
+        let angle = .pi / 6.0
         for index in 0...11 {
             timeButtons[index].transform = CGAffineTransform(rotationAngle: CGFloat((Double(index)*angle)+startAngle));
         }
         
 	}
-    
+    /*
     override func viewDidLayoutSubviews() {
-         self.removeContainerView()
+        self.removeContainerView()
         self.addHandsAndCenterPiece()
     }
-    
+    */
 	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 	}
 	
+    //ADD HERE !!
 	override func viewWillAppear(_ animated: Bool) {
 		super.viewWillAppear(animated)
-//        count += 1
-//        self.ViewLabel!.text = viewName + " appeared \(count) time" + ((count == 1) ? "" : "s")
+
         self.removeContainerView()
         self.addHandsAndCenterPiece()
+        
+        for i in 0...11 {
+            clockTasks[i].removeAll()
+        }
+        
+        //Tasks sort every time page is viewed
+        TaskManager.sharedTaskManager.sortTasks(by: .time)
+        let currTasks = TaskManager.sharedTaskManager.getTasks()
+        
+        //Current system time
+        var sysTime = Int32(Date().timeIntervalSince1970)
+        sysTime = sysTime - (sysTime%3600)
+        
+        for curr in currTasks {
+            let startTime = curr.getScheduleStart()
+            let endTime = curr.getDeadline()
+            //If beyond 12 hours, return
+            if startTime >= sysTime+(12*3600) {
+                break
+            }
+            
+            //at this point, task is within 12 hours
+            //Start index
+            let startIdx = (startTime-sysTime)/3600
+            //clockTasks[Int(startIdx)].append(curr)
+            
+            //End index
+            let endIdx = (endTime-1-sysTime)/3600
+            for i in startIdx...endIdx {
+                if i > 11 {
+                    break
+                }
+                clockTasks[Int(i)].append(curr)
+            }
+            
+        }
+     
+        /*for i in 0...11 {
+            for task in clockTasks[i] {
+                print(task.getTitle())
+            }
+            print("end")
+        }*/
+
 	}
 	
 	override func viewDidAppear(_ animated: Bool) {
 		super.viewDidAppear(animated)
-
 	}
     
     func addHandsAndCenterPiece() {
-        containerView = UIView(frame: CGRect(x: myClock.frame.midX, y: myClock.frame.midY, width: myClock.frame.width, height: myClock.frame.width))
+        containerView = UIView(frame: CGRect(x: myClock.frame.midX, y: myClock.frame.midY, width: myClock.frame.width/4, height: myClock.frame.width/4))
         
         containerView.tag = 10
         self.view.addSubview(containerView)
@@ -167,7 +361,7 @@ class ClockViewController: UIViewController, CAAnimationDelegate {
         containerView.layer.addSublayer(secondLayer)
         rotateLayer(secondLayer,dur: 60)
         
-        let endAngle = CGFloat(2*M_PI)
+        let endAngle = CGFloat(2) * .pi
         let circle = UIBezierPath(arcCenter: CGPoint(x: containerView.bounds.origin.x, y:containerView.bounds.origin.y), radius: myClock.bounds.height/2 * 0.03, startAngle: 0, endAngle: endAngle, clockwise: true)
         let centerPiece = CAShapeLayer()
         
@@ -221,10 +415,12 @@ class ClockViewController: UIViewController, CAAnimationDelegate {
         
         return (h:points[0],m:points[1],s:points[2])
     }
+	
     func degree2radian(_ a:CGFloat)->CGFloat {
-        let b = CGFloat(M_PI) * a/180
+        let b = .pi * a / 180.0
         return b
     }
+	
     func ctime ()->(h:Int,m:Int,s:Int) {
         var t = time_t()
         time(&t)
@@ -236,9 +432,11 @@ class ClockViewController: UIViewController, CAAnimationDelegate {
     @objc func applicationDidBecomeActive() {
         self.removeContainerView()
     }
+	
     @objc func applicationDidEnterBackground() {
         self.removeContainerView()
     }
+	
     func removeContainerView() {
         if (containerView != nil) {
             containerView.removeFromSuperview()
